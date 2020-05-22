91a0057510f388e82745b55af549db74173ab5bc
Sanjeev Yogi <yogi.sjv@gmail.com>
2020-05-22 04:58:28 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,5 +8,8 @@
 
 def select_winner(passengers)
   # add the code snippet here!
-  
+  passengers.each {|suite, name|
+    if suite == suite_a && name.start_with?("A")
+      winner = name 
+    }
 end
